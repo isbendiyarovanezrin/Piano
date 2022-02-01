25e3c7b3afbed04720ec2d66c422044d4686305a
Nəzrin <isbndyrva@gmail.com>
2022-02-02 01:30:57 +0400

    Add piano project

---
+++ js/piano.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.addEventListener("keydown", (e) => {
+  const key = document.querySelector(`.key[data-note="${e.key}"]`);
+  const audio = document.querySelector(`audio[data-note="${e.key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+});
+
+window.addEventListener("click", (e) => {
+  const sounds = document.querySelectorAll("audio[data-note]");
+  sounds.forEach((sound) => {
+    if (e.target.getAttribute("data-note") == sound.getAttribute("data-note")) {
+      sound.currentTime = 0;
+      sound.play();
+    }
+  });
+});
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeStyle));
+
+function removeStyle() {
+  this.classList.remove("playing");
+}
